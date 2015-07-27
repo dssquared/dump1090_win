97fbf398f6484f9e97c856bd19c8690963a65d48
Travis Painter <travis.painter@gmail.com>
2015-07-26 20:55:28 -0500

    Version number to 1.15-dev. Program name to dump1090-win.

--- dump1090.h
+++ dump1090.h
@@ -2,6 +2,24 @@
 //
 // dump1090.h: main program header
 //
+// Copyright (C) 2015  Travis Painter <travispainter@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// This file incorporates work covered by the following copyright and  
+// permission notices:
+//
 // Copyright (c) 2014,2015 Oliver Jowett <oliver@mutability.co.uk>
 //
 // This file is free software: you may copy, redistribute and/or modify it  
@@ -52,11 +70,11 @@
 
 // Default version number, if not overriden by the Makefile
 #ifndef MODES_DUMP1090_VERSION
-# define MODES_DUMP1090_VERSION     "v1.13-custom"
+# define MODES_DUMP1090_VERSION     "v1.15-dev"
 #endif
 
 #ifndef MODES_DUMP1090_VARIANT
-# define MODES_DUMP1090_VARIANT     "dump1090-mutability"
+# define MODES_DUMP1090_VARIANT     "dump1090-win"
 #endif
 
 // ============================= Include files ==========================
